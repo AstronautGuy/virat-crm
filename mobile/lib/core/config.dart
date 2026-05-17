88af46aeeafba63b3da05e8178cc6f4e4a5876d1
TheAstronautGuy <theastronautguy2@gmail.com>
2026-05-18 00:10:34 +0530

    chore: point mobile backend to production hosted Vercel address

--- mobile/lib/core/config.dart
+++ mobile/lib/core/config.dart
@@ -1,18 +1,4 @@
-import 'package:flutter/foundation.dart';
-import 'dart:io' show Platform;
-
 class AppConfig {
-  static String get baseUrl {
-    if (kReleaseMode) {
-      return 'https://virat-crm.vercel.app/api/rest';
-    }
-    if (kIsWeb) {
-      return 'http://localhost:3000/api/rest';
-    }
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:3000/api/rest';
-    }
-    return 'http://localhost:3000/api/rest';
-  }
+  static const String baseUrl = 'https://virat-crm.vercel.app/api/rest';
 }
 
